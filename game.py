a19ba59e8f6b0961fefca977368f8382e77d3849
Nguyen Khanh Anh N <73nguyen73@gmail.com>
2023-08-29 15:20:56 +0200

    added circle collection

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -7,7 +8,9 @@ pygame.init()
 # Constants
 WIDTH, HEIGHT = 800, 600
 SQUARE_SIZE = 50
+CIRCLE_RADIUS = 20
 SQUARE_COLOR = (255, 0, 0)
+CIRCLE_COLOR = (0, 0, 255)
 BACKGROUND_COLOR = (0, 0, 0)
 
 # Create the screen
@@ -20,6 +23,15 @@ x, y = WIDTH // 2 - SQUARE_SIZE // 2, HEIGHT // 2 - SQUARE_SIZE // 2
 # Initial movement direction
 dx, dy = 0, 0
 
+# List to store the circles
+circles = []
+
+# Counter for collected circles
+collected = 0
+
+# Create a font for the counter
+font = pygame.font.Font(None, 36)
+
 # Game loop
 running = True
 while running:
@@ -30,13 +42,13 @@ while running:
         # Keyboard input
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                dx = -1
+                dx = -0.5
             elif event.key == pygame.K_RIGHT:
-                dx = 1
+                dx = 0.5
             elif event.key == pygame.K_UP:
-                dy = -1
+                dy = -0.5
             elif event.key == pygame.K_DOWN:
-                dy = 1
+                dy = 0.5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 dx = 0
@@ -46,16 +58,37 @@ while running:
     # Update square position
     x += dx
     y += dy
-   
+
     # Bound checking to keep the square within the screen
     x = max(0, min(x, WIDTH - SQUARE_SIZE))
     y = max(0, min(y, HEIGHT - SQUARE_SIZE))
-    
+
+    # Randomly add circles to the list
+    if random.randint(1, 100) == 1:
+        circle_x = random.randint(0, WIDTH - CIRCLE_RADIUS * 2)
+        circle_y = random.randint(0, HEIGHT - CIRCLE_RADIUS * 2)
+        circles.append((circle_x, circle_y))
+
+    # Check for collision with circles
+    for circle in circles:
+        circle_x, circle_y = circle
+        if x < circle_x + CIRCLE_RADIUS and x + SQUARE_SIZE > circle_x and y < circle_y + CIRCLE_RADIUS and y + SQUARE_SIZE > circle_y:
+            circles.remove(circle)
+            collected += 1
+
     # Clear the screen
     screen.fill(BACKGROUND_COLOR)
 
+    # Draw the circles
+    for circle in circles:
+        pygame.draw.circle(screen, CIRCLE_COLOR, circle, CIRCLE_RADIUS)
+
     # Draw the square
     pygame.draw.rect(screen, SQUARE_COLOR, (x, y, SQUARE_SIZE, SQUARE_SIZE))
+
+    # Display the collected count
+    text = font.render(f"Collected: {collected}", True, (255, 255, 255))
+    screen.blit(text, (10, 10))
 
     # Update the screen
     pygame.display.flip()
